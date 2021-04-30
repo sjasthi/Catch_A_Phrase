5e45f7324abb3e2cc7f8ea0e76dea9ce87345931
SamaraGarrett <samaraegarrett@gmail.com>
2021-04-29 20:31:23 -0500

    Enhancements
    - Added a text field called "Phrase" to show the phrase as-is, and another text field called "Processed Phrase" to show the phrase as characters separated by commas. Processed Phrase also strips and blanks, spaces, or punctuation.
    - Added a default quote "తెలుగు పజిల్స్".
    - Added labels, A to Z across and 1 to N down.
    - Added a show/hide solution button with the default being that the solution is hidden upon generation.
    - Chose a lighter color for the solution highlight so that the solution characters would be easier to read.
    - Made some stylistic changes.

---
+++ header.php
@@ -0,0 +1,19 @@
+<!--
+Class: ICS 325 Section 01 Summer 2019
+Name: Samara Garrett
+Assignment: Assignment 2
+
+Reference:
+ https://stackoverflow.com/questions/3518002/how-can-i-set-the-default-value-for-an-html-select-element
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        <?php echo $title ?>
+    </title>
+    <link type="text/css" media="all" href="phrase_style.css" rel="stylesheet" />
+    <script src="phrase_scripts.js"></script>
+</head>
+<body>
